4da8654ef1de8383faaac99c3ed217b73e211e60
sangoon <snkorp@gmail.com>
2023-04-27 18:42:38 +0000

    Pending changes exported from your codespace

--- src/background/contextMenus.ts
+++ src/background/contextMenus.ts
@@ -1,59 +1,67 @@
-import { getMutableStateSingleton } from "./backgroundState";
 import { notify } from "../common/notify";
+import { getMutableStateSingleton } from "./backgroundState";
 
-import { addDownloadTasksAndPoll } from "./actions";
 import { ALL_DOWNLOADABLE_PROTOCOLS, startsWithAnyProtocol } from "../common/apis/protocols";
+import { addDownloadTasksAndPoll } from "./actions";
+
+
+function toUrls({linkUrl, srcUrl, selectionText}:ContextMenusOnClickData) {
+  
+}
+
+function clickHandler(data: ContextMenusOnClickData): void {
+  const state = getMutableStateSingleton();
+
+  if (data.linkUrl) {
+    addDownloadTasksAndPoll(
+      state.api,
+      state.pollRequestManager,
+      state.showNonErrorNotifications,
+      [data.linkUrl],
+    );
+  }
+  if (data.srcUrl) {
+    addDownloadTasksAndPoll(
+      state.api,
+      state.pollRequestManager,
+      state.showNonErrorNotifications,
+      [data.srcUrl],
+    );
+  } 
+  if (data.selectionText) {
+    let urls = data.selectionText
+      .split("\n")
+      .map((url) => url.trim())
+      // The cheapest of checks. Actual invalid URLs will be caught later.
+      .filter((url) => startsWithAnyProtocol(url, ALL_DOWNLOADABLE_PROTOCOLS));
+
+    if (urls.length == 0) {
+      notify(
+        browser.i18n.getMessage("Failed_to_add_download"),
+        browser.i18n.getMessage("Selected_text_is_not_a_valid_URL"),
+        "failure",
+      );
+      return;
+    } 
+      addDownloadTasksAndPoll(
+        state.api,
+        state.pollRequestManager,
+        state.showNonErrorNotifications,
+        urls,
+      );
+      return;
+  } 
+  notify(
+    browser.i18n.getMessage("Failed_to_add_download"),
+    browser.i18n.getMessage("URL_is_empty_or_missing"),
+    "failure",
+  );
+}
 
 export function initializeContextMenus() {
   browser.contextMenus.create({
     enabled: true,
     title: browser.i18n.getMessage("Download_with_DownloadStation"),
     contexts: ["link", "audio", "video", "image", "selection"],
-    onclick: (data) => {
-      const state = getMutableStateSingleton();
-
-      if (data.linkUrl) {
-        addDownloadTasksAndPoll(
-          state.api,
-          state.pollRequestManager,
-          state.showNonErrorNotifications,
-          [data.linkUrl],
-        );
-      } else if (data.srcUrl) {
-        addDownloadTasksAndPoll(
-          state.api,
-          state.pollRequestManager,
-          state.showNonErrorNotifications,
-          [data.srcUrl],
-        );
-      } else if (data.selectionText) {
-        let urls = data.selectionText
-          .split("\n")
-          .map((url) => url.trim())
-          // The cheapest of checks. Actual invalid URLs will be caught later.
-          .filter((url) => startsWithAnyProtocol(url, ALL_DOWNLOADABLE_PROTOCOLS));
-
-        if (urls.length == 0) {
-          notify(
-            browser.i18n.getMessage("Failed_to_add_download"),
-            browser.i18n.getMessage("Selected_text_is_not_a_valid_URL"),
-            "failure",
-          );
-        } else {
-          addDownloadTasksAndPoll(
-            state.api,
-            state.pollRequestManager,
-            state.showNonErrorNotifications,
-            urls,
-          );
-        }
-      } else {
-        notify(
-          browser.i18n.getMessage("Failed_to_add_download"),
-          browser.i18n.getMessage("URL_is_empty_or_missing"),
-          "failure",
-        );
-      }
-    },
-  });
+    onclick: clickHandler);
 }
